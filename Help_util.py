af36e3d487c4d4418b4f8a0a057d95b655cbd18f
lena55a <114000158+lena55a@users.noreply.github.com>
2024-09-30 14:09:13 +0300

    Update Help_util.py

--- Help_util.py
+++ Help_util.py
@@ -9,7 +9,7 @@ import json
 
 # Load the Google vision model:
 # Path to your JSON configuration file
-json_path = "Change on run time" # I sent the json file in the group, btw its private.
+json_path = "/content/speed-detection-with-cctv-ai-f48de7401899.json" # I sent the json file in the group, btw its private.
 
 # Load configuration from JSON file
 with open(json_path, 'r') as file:
